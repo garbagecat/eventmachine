0e7cdda2cb106fb1b41b2053b9cfddef9c77d771
raggi <raggi@2c60eb90-88cd-488e-9351-1edcb1d70dbc>
2008-05-20 17:27:33 +0000

    * Removed a delegate in DefaultConnectionFactory - could be wrong, but compiled. TODO.  * Major refactor of Rakefile, will need opinions on that.  * Imported generic rake tasks from elsewhere  * Moved all test tasks to a tests.rake import.  * Added a real jar and jgem task chain.
    git-svn-id: http://rubyeventmachine.com/svn/branches/raggi@691 2c60eb90-88cd-488e-9351-1edcb1d70dbc

--- java/src/com/rubyeventmachine/DefaultConnectionFactory.java
+++ java/src/com/rubyeventmachine/DefaultConnectionFactory.java
@@ -39,7 +39,7 @@ public class DefaultConnectionFactory implements ConnectionFactory {
 	 * Connection, which is usually overridden. This class is probably most
 	 * useful for unit testing.
 	 */
-	@Override
+//	@Override
 	public Connection connection() {
 		return new Connection();
 	}
